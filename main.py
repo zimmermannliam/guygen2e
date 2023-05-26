e3efd2128a1681e32f2048e76662ff143c19c9c7
Liam Zimmermann <zimmerml@oregonstate.edu>
2023-05-26 14:57:13 -0700

    add bang operation to run last command

--- main.py
+++ main.py
@@ -9,15 +9,24 @@ def main():
 def monitor():
     """ Run monitor in a loop, taking user input and running the corresponding cmd"""
     ret = None
+    last_cmd = ""
     while ret != cmd.QUIT:
         cmd_str = input("> ")
+        
+
+        # whitespace input
+        if cmd_str.strip() == "":
+            continue
+        
+        # ! input
+        temp_cmd = cmd_str
+        if cmd_str.strip() == "!":
+            cmd_str = last_cmd
+        else:
+            last_cmd = temp_cmd
 
         cmd_args = cmd_str.split()
 
-        # whitespace input
-        if not cmd_args:
-            continue
-        
         if cmd_args[0] in cmd.cmd_alt_names:
             cmd_args[0] = cmd.cmd_alt_names[cmd_args[0]]
 
